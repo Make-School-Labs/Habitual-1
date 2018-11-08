0c8d20ab8b4425d65b872860ce569fa0f6456b28
LinnierGames <esericksanc@gmail.com>
2018-11-08 12:12:51 -0800

    WIP: PersistanceLayer
    Habit.Images stores to Int values

--- Habitual/Habit.swift
+++ Habitual/Habit.swift
@@ -10,8 +10,8 @@ import Foundation
 import UIKit.UIImage
 
 struct Habit {
-    enum Images {
-        case outdoors
+    enum Images: Int, CaseIterable {
+        case outdoors = 0
         
         var image: UIImage {
             guard let image = UIImage(named: String(describing: self)) else {
---
+++ Habitual/PersistenceLayer.swift
@@ -0,0 +1,9 @@
+//
+//  PersistenceLayer.swift
+//  Habitual
+//
+//  Created by Erick Sanchez on 11/8/18.
+//  Copyright © 2018 Sam Galizia. All rights reserved.
+//
+
+import Foundation
